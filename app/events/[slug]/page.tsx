0c340f3a8038862b76342d6b311ee637d5e22e1d
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2025-11-06 17:26:43 +0700

    fix

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,15 +1,11 @@
 import EventDetails from "@/components/EventDetails";
 import { Suspense } from "react";
 
-export const dynamic = 'force-dynamic';
-
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const slug = params.then((p) => p.slug);
-
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
-        <EventDetails params={slug} />
+        <EventDetails params={params.then((p) => p.slug)} />
       </Suspense>
     </main>
   );
